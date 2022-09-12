5a97c7b704ead30074a313a28d7804565b843c91
AndreyKlimmov <andrey.klimmov@gmail.com>
2022-09-12 13:29:14 +0300

    draft hw9 task1 registration page

--- src/app/core/pages/registration-page/registration-page.component.ts
+++ src/app/core/pages/registration-page/registration-page.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {passwordValidator} from "../../validators/auth.validators";
+import {Router} from "@angular/router";
 
 const EMAIL_PATTERN = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+const NAME_PATTERN = new RegExp(/(^[a-zA-Z][a-zA-Z\s]{0,20}[a-zA-Z]$)/)
+const PASSWORD_PATTERN = new RegExp(/(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=(.*\d){3,})(?=.{6,}).*$/)
 
 @Component({
   selector: 'app-registration-page',
@@ -13,36 +16,58 @@ export class RegistrationPageComponent implements OnInit {
   // public emailControl!: FormControl;
   // public passwordControl!: FormControl;
   // public confirmPasswordControl!: FormControl;
+  //@Input('email-invalid')
 
   public registrationForm!: FormGroup;
+  //public test: any = this.registrationForm.controls['email']['status'] || ''
 
   constructor(
     private fb: FormBuilder,
+    private router: Router,
   ) { }
 
   public ngOnInit(): void {
+
     this.initializeForm()
+    //const test: any = this.registrationForm.controls['email']['status']
     //this.emailControl.valueChanges.subscribe(console.log)
     this.registrationForm.valueChanges.subscribe(() =>
-    {console.log('reg form', this.registrationForm)})
+    {console.log('reg form:', this.registrationForm, 'status:',
+      )})
   }
 
-  public registration(): void {
+  public registration(): any {
     // console.log('emailControl', this.emailControl);
     // console.log('emailControl.value', this.emailControl.value);
     if (this.registrationForm.valid) {
       const payLoad = {
         email: this.registrationForm.get('email')?.value,
         password: this.registrationForm.get('password')?.value,
-        confirmPassword: this.registrationForm.get('confirmPassword')?.value
+        //confirmPassword: this.registrationForm.get('confirmPassword')?.value,
+        name: this.registrationForm.get('name')?.value
 
         // email: this.emailControl.value,
         // password: this.passwordControl.value,
         // confirmPassword: this.confirmPasswordControl.value
       }
       console.log('payLoad', payLoad);
-      const registeredUser = JSON.stringify(payLoad)
-      localStorage.setItem('user', registeredUser)
+      let registeredUsers: any = []
+      if (localStorage.getItem('users')) {
+        registeredUsers = JSON.parse(localStorage.getItem('users') || '')
+        const authUsers = registeredUsers.find((user: any) => user.email === payLoad.email);
+        if (authUsers) {
+          //console.log('authUsers:', authUsers);
+          //localStorage.setItem('session', JSON.stringify(authUsers))
+          return this.router.navigate(['/login'])
+        }
+      }
+        console.log('registeredUsers', registeredUsers);
+        registeredUsers.push(payLoad)
+        localStorage.setItem('users', JSON.stringify(registeredUsers))
+
+        this.router.navigate(['/login'])
+
+
     }
   }
 
@@ -54,8 +79,23 @@ export class RegistrationPageComponent implements OnInit {
     this.registrationForm = this.fb.group({
       email: new FormControl<any>('',
         [Validators.required, Validators.pattern(EMAIL_PATTERN)]),
-      password: new FormControl<any>('', [Validators.required]),
-      confirmPassword: new FormControl<any>('', [Validators.required])
+      password: new FormControl<any>('', [Validators.required, Validators.pattern(PASSWORD_PATTERN)]),
+      confirmPassword: new FormControl<any>('', [Validators.required, Validators.pattern(PASSWORD_PATTERN)]),
+      name: new FormControl<any>('',
+        [Validators.required, Validators.pattern(NAME_PATTERN)]),
+
     }, {validators: [passwordValidator]})
+  }
+
+  public getClassEmail(string: string): string {
+    if (this.registrationForm.hasError('error')) {
+      if (string === 'confirmPassword') {
+        return 'invalid'
+      }
+    }
+    if (this.registrationForm.controls[string]['status'] === 'INVALID') {
+      return 'invalid'
+    }
+    return ''
   }
 }
