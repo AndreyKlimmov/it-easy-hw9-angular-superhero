b47915e4ada0527a2f8dc691d12b807af816c1a9
AndreyKlimmov <andrey.klimmov@gmail.com>
2022-09-17 01:27:27 +0300

    task6 hero-list in local storage

--- src/app/core/components/hero-card/hero-card.component.ts
+++ src/app/core/components/hero-card/hero-card.component.ts
@@ -11,17 +11,49 @@ export class HeroCardComponent implements OnInit {
   @Input() disableViewBtn: string = '';
   @Input() disableSelectBtn: string = '';
   @Input() disableName: string = '';
+  @Input() getReady: boolean = false;
+  public select: string = 'Select';
+  public gotReady: boolean = false;
+  public heroList: any[] = [];
+  public user: any;
 
   constructor(
     private router: Router,
   ) { }
 
   ngOnInit(): void {
+    this.initializeVar()
     console.log('hero:', this.hero);
   }
 
-  public navToPage(pagePath: any): void {
-    this.router.navigate(['/' + pagePath])
+  private initializeVar(): void {
+    this.getReady ? this.select = 'Get Ready' : this.select
+
+    if (localStorage.getItem('session')) {
+      this.user = JSON.parse(localStorage.getItem('session') || '')
+    }
+    if (localStorage.getItem(`${this.user.email}`)) {
+      this.heroList = JSON.parse(localStorage.getItem(`${this.user.email}`) || '')
+      const hero = this.heroList.find(
+        (heroId: any) => heroId === this.hero.id);
+      if (hero) {this.disableSelectBtn = 'selectedHero'}
+    }
+  }
+
+  public selectHero(): void {
+
+    if (this.disableSelectBtn !== 'selectedHero') {
+      if (localStorage.getItem(`${this.user.email}`) || '') {
+        this.heroList = JSON.parse(localStorage.getItem(`${this.user.email}`) || '')
+      }
+
+      this.heroList.push(this.hero.id)
+      localStorage.setItem(`${this.user.email}`, JSON.stringify(this.heroList))
+      this.disableSelectBtn = 'selectedHero'
+    }
+
+
+
   }
 
   public navToHeroDetailsPage(hero: any): void {
